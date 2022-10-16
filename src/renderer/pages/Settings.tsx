de1497bd1fa723b5cad5c2165390fa80caab5be1
sc <stevencstuy@gmail.com>
2022-10-16 15:10:03 -0400

    Add settings form

--- src/renderer/pages/Settings.tsx
+++ src/renderer/pages/Settings.tsx
@@ -1,3 +1,70 @@
+import { useRef, useState } from 'react';
+
 export const Settings = () => {
-  return <div>Settings</div>;
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const [globalRpc, setGlobalRpc] = useState('');
+  const [etherscanKey, setEtherscanKey] = useState('');
+  const [discordWebhook, setDiscordWebhook] = useState('');
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      <div className="text-2xl mb-8">Settings</div>
+      <form ref={formRef}>
+        <div className="mb-4">
+          <label htmlFor="rpc" className="flex flex-col gap-1">
+            Global RPC
+            <input
+              type="text"
+              name="rpc"
+              id="rpc"
+              placeholder="RPC Link"
+              value={globalRpc}
+              onChange={(e) => setGlobalRpc(e.target.value)}
+              className="w-[512px] h-8 p-2 mr-2 rounded-md bg-gray-600 hover:bg-gray-500 focus:bg-gray-500"
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="etherscan" className="flex flex-col gap-1">
+            Etherscan API Key
+            <input
+              type="text"
+              name="etherscan"
+              id="etherscan"
+              placeholder="API Key"
+              value={etherscanKey}
+              onChange={(e) => setEtherscanKey(e.target.value)}
+              className="w-[512px] h-8 p-2 mr-2 rounded-md bg-gray-600 hover:bg-gray-500 focus:bg-gray-500"
+            />
+          </label>
+        </div>
+        <div className="mb-4">
+          <label htmlFor="webhook" className="flex flex-col gap-1">
+            Discord Webhook
+            <input
+              type="text"
+              name="webhook"
+              id="webhook"
+              placeholder="Discord Webhook URL"
+              value={discordWebhook}
+              onChange={(e) => setDiscordWebhook(e.target.value)}
+              className="w-[512px] h-8 p-2 mr-2 rounded-md bg-gray-600 hover:bg-gray-500 focus:bg-gray-500"
+            />
+          </label>
+        </div>
+        <button
+          type="submit"
+          onClick={handleSubmit}
+          className="border rounded-md px-4 py-1.5 hover:bg-gray-600"
+        >
+          Save
+        </button>
+      </form>
+    </div>
+  );
 };
