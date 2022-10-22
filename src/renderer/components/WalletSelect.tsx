57bf2c4a3589f8cc03052283c9e1681e6d43b067
sc <stevencstuy@gmail.com>
2022-10-22 14:50:13 -0400

    Change wallet select to handle single choices
    When a user is creating a task, the wallet select component should offer
    multiple wallet choices and create a task for each wallet. However, when
    a user is editing a task, they should only be able to select a single
    wallet and update the task wallet. It would not make sense for users to
    edit a single wallet into multiple.
    
    This change removes the 'multiple' property in the event that a user is
    editing a task rather than creating a task.

--- src/renderer/components/WalletSelect.tsx
+++ src/renderer/components/WalletSelect.tsx
@@ -3,29 +3,68 @@ import { IWallet } from 'interfaces/IWallet';
 
 interface WalletSelectProps {
   fetchedWallets: IWallet[];
-  selectedWallets: IWallet[];
-  setSelectedWallets: React.Dispatch<React.SetStateAction<IWallet[]>>;
+  // if edit task modal is open, wallet select should only allow one wallet choice
+  selectedWallet?: IWallet;
+  setSelectedWallet?: React.Dispatch<React.SetStateAction<IWallet>>;
+
+  // if add task modal is open, wallet select should allow multiple wallet choices
+  selectedWallets?: IWallet[];
+  setSelectedWallets?: React.Dispatch<React.SetStateAction<IWallet[]>>;
 }
 
 export const WalletSelect = ({
   fetchedWallets,
+  selectedWallet,
+  setSelectedWallet,
   selectedWallets,
   setSelectedWallets,
 }: WalletSelectProps) => {
+  if (selectedWallets && setSelectedWallets) {
+    // add task modal is open
+    return (
+      <Listbox
+        value={selectedWallets}
+        onChange={setSelectedWallets}
+        name="wallets"
+        multiple
+      >
+        <div className="relative mt-1">
+          <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left">
+            <span className="block truncate">
+              {selectedWallets.length > 0
+                ? selectedWallets.map((wallet) => wallet.name).join(', ')
+                : 'Select one or more wallets'}
+            </span>
+          </Listbox.Button>
+          <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg">
+            {fetchedWallets.map((wallet) => (
+              <Listbox.Option
+                key={wallet.address}
+                className={({ active, selected }) =>
+                  `relative cursor-default select-none py-2 pl-2 pr-4 hover:bg-gray-300 ${
+                    active ? 'bg-gray-300' : 'text-gray-900'
+                  } ${selected ? 'bg-gray-300' : 'bg-white'}`
+                }
+                value={wallet}
+              >
+                {wallet.name}
+              </Listbox.Option>
+            ))}
+          </Listbox.Options>
+        </div>
+      </Listbox>
+    );
+  }
+
+  // edit task modal is open
   return (
-    <Listbox
-      value={selectedWallets}
-      onChange={setSelectedWallets}
-      name="wallets"
-      multiple
-    >
+    // remove multiple attribute
+    <Listbox value={selectedWallet} onChange={setSelectedWallet} name="wallets">
       <div className="relative mt-1">
         <Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left">
-          <span className="block truncate">
-            {selectedWallets.length > 0
-              ? selectedWallets.map((wallet) => wallet.name).join(', ')
-              : 'Select one or more wallets'}
-          </span>
+          {/* this will be defined but added ? to satisfy type check */}
+          {/* only show wallet name instead of 'Select one or more wallets' / join with commas */}
+          <span className="block truncate">{selectedWallet?.name}</span>
         </Listbox.Button>
         <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg">
           {fetchedWallets.map((wallet) => (
@@ -45,4 +84,11 @@ export const WalletSelect = ({
       </div>
     </Listbox>
   );
+};
+
+WalletSelect.defaultProps = {
+  selectedWallet: null,
+  setSelectedWallet: null,
+  selectedWallets: null,
+  setSelectedWallets: null,
 };
